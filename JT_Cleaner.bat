c523eba5e07094dd8523bc8a3b5f0b48d60949b5
JuanTech-cyber <62424431+JuanTech-cyber@users.noreply.github.com>
2021-04-04 20:28:26 +0200

    Add files via upload

---
+++ JT_Cleaner.bat
@@ -0,0 +1,105 @@
+@echo off
+title JT_Cleaner
+color e 
+mode 150,60
+:home
+echo `````````````````````````````````````......---------::::::::://////////////::--..````````````````````````````````````````````````````
+echo `````````````````````````````````````````````````.:/++ooooooooooooooooooooooooooooooooooooooooooo++:.`````````````````````````````````````````````````
+echo ``````````````````````````````````````````````./+ooooooooooooooooooooooooooooooooooooooooooooooooooo+/.```````````````````````````````````````````````
+echo ````````````````````````````````````````````./oooooooooooooooooooooooooooooooooooooooooooooooooooooooo+:``````````````````````````````````````````````
+timeout /t 1 > nul
+echo ```````````````````````````````````````````-+ooooooooooooooooooooooooooooooooooooooooooooooooooooooooooo/.````````````````````````````````````````````
+echo ``````````````````````````````````````````:oooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooo+:```````````````````````````````````````````
+echo ````````````````````````````````````````./ooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooo/.`````````````````````````````````````````
+echo ```````````````````````````````````````./ooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooo+-```````````.````````````````````````````
+echo ``````````````````````````````````````./oooooooooooooooooooooooooooooooooo/ooooooooooooooooooooooooooooooooooo-``````.-:/:````````````````````````````
+echo `````````````````````````````````````./oooooooooooooooooooooooooooooooooo-`-+oooooooooooooooooooooooooooooooooo:..-:/++o/.````````````````````````````
+echo ````````````````````````````````````./ooooooooooooooooooooooooooooooooo+-```-+oooooooooooooooooooooooooooooooooo++ooooo/.`````````````````````````````
+echo ```````````````````````````````````./ooooooooooooooooooooooooooooooooo+-`````./ooooooooooooooooooooooooooooooooooooooo/.``````````````````````````````
+echo ``````````````````````````````````./ooooooooooooooooooooooooooooooooo+-```````./ooooooooooooooooooooooooooooooooooooo+.```````````````````````````````
+timeout /t 1 > nul
+echo `````````````````````````````````./ooooooooooooooooooooooooooooooooo+-``````````:ooooooooooooooooooooooooooooooooooo+.````````````````````````````````
+echo ````````````````````````````````./ooooooooooooooooooooooooooooooooo+-````````````-ooooooooooooooooooooooooooooooooo+.`````````````````````````````````
+echo ````````````````````````````````/ooooooooooooooooooooooooooooooooo+-``````````````-+oooooooooooooooooooooooooooooo+.``````````````````````````````````
+echo ````````````````````````````````-:/+ooooooooooooooooooooooooooooo+-````````````````./oooooooooooooooooooooooooooo+.```````````````````````````````````
+echo ```````````````````````````````````.-:/+oooooooooooooooooooooooo+-```````````````````:oooooooooooooooooooooooooo+.````````````````````````````````````
+echo ```````````````````````````````````````.-:/+ooooooooooooooooooo+-`````````````````````:oooooooooooooooooooooooo+.```````.-/-``````````````````````````
+echo ````````````````````````````````````````````-:/+oooooooooooooo+.```````````````````.-/oooooooooooooooooooooooo+.`````.:/+ooo-`````````````````````````
+echo ```````````````````````````````````````````````.-:/+ooooooooo+.`````````````````.-/+ooooooooo+oo+o++++++++++++.```.:+oooooooo:````````````````````````
+timeout /t 1 > nul
+echo ````````````````.-:::::::::::::::::::::::::::``````.-:++oooo+.``````````````````-::-------..........```````````.:+oooooooooooo:```````````````````````
+echo ``````````````````-:+oooooooooooooooooooooooo/.````````.-/++.```````````````````````````````````````````````.:+oooooooooooooooo/``````````````````````
+echo `````````````````````.:/oooooooooooooooooooooo+.`````````````````````````````````````````````````````````.:+oooooooooooooooooooo/.````````````````````
+echo ```````````````````````-oooooooooooooooooooooooo-`````````````````````````````````````````````````````.:+oooooooooooooooooooooooo+.```````````````````
+echo ``````````````````````:oooooooooooooooooooooooooo-`````````````````````````````````````````````````-/+oooooooooooooooooooooooooooo+.``````````````````
+echo ````````````````````./oooooooooooooooooooooooooooo:``````````````````````````````````````````````-ooooooooooooooooooooooooooooooooo+.`````````````````
+echo ```````````````````.+oooooooooooooooooooooooooooooo/.`````````````````````````````````````````````/ooooooooooooooooooooooooooooooooo+.````````````````
+echo ``````````````````.+oooooooooooooooooooooooooooooooo+.`````````````````````````````````````````````:ooooooooooooooooooooooooooooooooo+````````````````
+echo `````````````````-+oooooooooooooooooooooooooooooooooo+-`````````````````````````````````````````````:ooooooooooooooooooooooooooooooooo/```````````````
+timeout /t 1 > nul
+echo ````````````````-+ooooooooooooooooooooooooooooooooooooo:`````````````````````````````````````````````-ooooooooooooooooooooooooooooooooo-``````````````
+echo ```````````````.+oooooooooooooooooooooooooooooooo+:/+ooo/`````````````````````````````````````````````-+ooooooooooooooooooooooooooooooo:``````````````
+echo ```````````````-oooooooooooooooooooooooooooooooo+.```.-://.````````````````````````````````````````````.+oooooooooooooooooooooooooooooo-``````````````
+echo ```````````````/ooooooooooooooooooooooooooooooo/.````````..`````````````````````````````````````````````.+oooooooooooooooooooooooooooo/```````````````
+echo ```````````````/oooooooooooooooooooooooooooooo:``````````````````````````````````````````````````````````.+oooooooooooooooooooooooooo+.```````````````
+echo ```````````````-oooooooooooooooooooooooooooo+-````````````````````````````````````````````.```````````````.+ooooooooooooooooooooooooo-````````````````
+echo ````````````````:ooooooooooooooooooooooooo++.````````````````````````````````````````````:/````````````````./ooooooooooooooooooooooo:`````````````````
+echo `````````````````/ooooooooooooooooooooooo+/.````````````````````````````````````````````:o/`````````````````./ooooooooooooooooooooo:``````````````````
+echo ``````````````````:ooooooooooooooooooooo+:`````````````````````````````````````````````/oo+``````````````````./ooooooooooooooooooo:```````````````````
+echo ```````````````````-+oooooooooooooooooo+:`````````````````````````````````````````````/ooo+```````````````````./oooooooooooooooo+:````````````````````
+echo ````````````````````-+ooooooooooooooooo+------------------------------.`````````````./ooooo+++++++++++++++++++++oooooooooooooooo:`````````````````````
+echo `````````````````````./oooooooooooooooooooooo+++++ooooooo++ooooooooooo+````````````./oooooooooooooooooooooooooooooooooooooooooo:``````````````````````
+timeout /t 1 > nul
+echo ```````````````````````:oooooooooooooooooooooooooooooooooooooooooooooo+```````````./oooooooooooooooooooooooooooooooooooooooooo:```````````````````````
+echo ````````````````````````-+ooooooooooooooooooooooooooooooooooooooooooooo``````````./oooooooooooooooooooooooooooooooooooooooooo/````````````````````````
+echo `````````````````````````.+oooooooooooooooooooooooooooooooooooooooooooo.````````./oooooooooooooooooooooooooooooooooooooooooo/`````````````````````````
+echo ``````````````````````````./ooooooooooooooooooooooooooooooooooooooooooo.```````./ooooooooooooooooooooooooooooooooooooooooo+/``````````````````````````
+echo ````````````````````````````:+ooooooooooooooooooooooooooooooooooooooooo.``````./oooooooooooooooooooooooooooooooooooooooooo/```````````````````````````
+echo `````````````````````````````-+oooooooooooooooooooooooooooooooooooooooo.`````./oooooooooooooooooooooooooooooooooooooooooo/````````````````````````````
+echo ``````````````````````````````./ooooooooooooooooooooooooooooooooooooooo-`````:oooooooooooooooooooooooooooooooooooooooooo/`````````````````````````````
+echo ````````````````````````````````:oooooooooooooooooooooooooooooooooooooo-``````:+oooooooooooooooooooooooooooooooooooo+o+/``````````````````````````````
+echo `````````````````````````````````-+oooooooooooooooooooooooooooooooooooo-```````:+oooooooooooooooooooooooooooooooooooo+:```````````````````````````````
+echo ``````````````````````````````````.+ooooooooooooooooooooooooooooooooooo:````````-+oooooooooooooooooooooooooooooooooo+:````````````````````````````````
+timeout /t 1 > nul
+echo ````````````````````````````````````:+ooooooooooooooooooooooooooooooooo/`````````-+oooooooooooooooooooooooooooooooo+-`````````````````````````````````
+echo `````````````````````````````````````-/oooooooooooooooooooooooooooooooo/``````````.+oooooooooooooooooooooooo+oooo+:.``````````````````````````````````
+echo ```````````````````````````````````````-/+ooo+ooooooooooooooooooooooooo/```````````./ooo+ooo+++ooooo+ooo+o+++++/-.````````````````````````````````````
+echo `````````````````````````````````````````.:/++++++++++++o+o+++++oooo+++/`````````````/oo+oo:----::::::::::--..``
+timeout /t 3 > nul
+cls
+echo.
+echo ///////////////////////////////////////////////
+echo ----------- Starting JT_Cleaner ---------------
+echo ------------------ Please ---------------------
+echo ..... Wait a moment and select the option......
+echo /////////////////////////////////////////////// 
+echo.
+echo ###############################################
+echo ====  1. Clean
+echo ====  2. Exit
+echo ###############################################
+echo.
+echo $$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$
+echo ************* If this does not work ************
+echo ********** Please run as administrator**********
+echo $$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$
+echo.
+set /p menu=Option=
+
+if "%menu%"=="1" goto op1
+if "%menu%"=="2" goto op2
+
+:op1 
+cls 
+echo.
+net stop wuauserv 
+cd %Windir%
+cd SoftwareDistribution 
+del /f/s/q Download 
+net start wuauserv
+pause 
+cls
+goto home
+
+:op2
+cls&exit
